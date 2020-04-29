ddfc56ab3d77b0b68ccb7e1fd4ab149489c12b70
Anastasia Orlova <metkusblackus@gmail.com>
2020-04-29 19:35:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,6 +13,7 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   array.each_with_index do |character, index|
-    p "#{character[index]}"
+    array[character] = index 
+    p index + ". #{character}"
   end
 end
