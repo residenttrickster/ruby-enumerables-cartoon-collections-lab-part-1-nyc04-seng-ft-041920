4503616bd67350a93fe865a091614cbdbd7cbd17
Anastasia Orlova <metkusblackus@gmail.com>
2020-04-29 19:22:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,12 +6,6 @@ def greet_characters(array)
   array.each do |character|
     p "Hello #{character}!"
   }
-  end
-  
+  end  
 end
 
-def list_dwarves(array)
-  # Use `each_with_index` to enumerate over the provided array
-  #
-  # Print a numbered list of each element
-end
